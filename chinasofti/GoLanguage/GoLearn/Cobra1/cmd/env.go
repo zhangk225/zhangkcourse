1b8abd750db17f165fee60b54821a0fbcd882a1e
rzhang2 <Ray.Zhang@activenetwork.com>
2024-04-02 14:59:28 +0800

    add env command to store selected env

--- chinasofti/GoLanguage/GoLearn/Cobra1/cmd/env.go
+++ chinasofti/GoLanguage/GoLearn/Cobra1/cmd/env.go
@@ -26,15 +26,10 @@ to quickly create a Cobra application.`,
 }
 
 func init() {
+	addSubCommand()
 	rootCmd.AddCommand(envCmd)
+}
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// envCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// envCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func addSubCommand() {
+	envCmd.AddCommand(env.ShowEnvCmd)
 }
--- chinasofti/GoLanguage/GoLearn/Cobra1/cmd/env/showEnv.go
+++ chinasofti/GoLanguage/GoLearn/Cobra1/cmd/env/showEnv.go
@@ -1,7 +1,7 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 */
-package cmd
+package env
 
 import (
 	"fmt"
@@ -11,7 +11,7 @@ import (
 )
 
 // showEnvCmd represents the showEnv command
-var showEnvCmd = &cobra.Command{
+var ShowEnvCmd = &cobra.Command{
 	Use:   "showEnv",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
@@ -21,14 +21,14 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		ee := viper.GetString("env")
-		fmt.Println("showEnv called" + ee)
+		envirnment := viper.GetString("env")
+		url := GetCurrentEnvPmAPIBaseURL()
+		fmt.Printf("env is %s; url is %s", envirnment, url)
 
 	},
 }
 
 func init() {
-	envCmd.AddCommand(showEnvCmd)
 
 	// Here you will define your flags and configuration settings.
 
