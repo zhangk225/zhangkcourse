07881274a548374e7d5ba7e88df2d342d6b75c21
rzhang2 <Ray.Zhang@activenetwork.com>
2024-04-02 14:30:17 +0800

    add env command to store selected env

--- chinasofti/GoLanguage/GoLearn/Cobra1/cmd/root.go
+++ chinasofti/GoLanguage/GoLearn/Cobra1/cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 Ray Zhang Ray.Zhang@activenetwork.com
-
 */
 package cmd
 
@@ -12,18 +11,11 @@ import (
 	"github.com/spf13/viper"
 )
 
-var cfgFile string
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "Cobra1",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "imitative to payment-manager-vendor-cli",
+	Long:  `learning how to develop a CLI application`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -41,12 +33,6 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.Cobra1.yaml)")
-
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
@@ -54,24 +40,26 @@ func init() {
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
+	home, err := os.UserHomeDir()
+	cobra.CheckErr(err)
 
-		// Search config in home directory with name ".Cobra1" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".Cobra1")
-	}
+	// Search config in home directory with name ".Cobra1" (without extension).
+	viper.AddConfigPath(home)
+	viper.SetConfigType("yaml")
+	viper.SetConfigName(".Cobra1")
 
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			_ = viper.SafeWriteConfig()
+			_ = viper.ReadInConfig()
+			fmt.Println("created config file:", viper.ConfigFileUsed())
+		}
 	}
 }
---
+++ chinasofti/GoLanguage/GoLearn/Cobra1/cmd/showEnv.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// showEnvCmd represents the showEnv command
+var showEnvCmd = &cobra.Command{
+	Use:   "showEnv",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		ee := viper.GetString("env")
+		fmt.Println("showEnv called" + ee)
+
+	},
+}
+
+func init() {
+	envCmd.AddCommand(showEnvCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// showEnvCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// showEnvCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
